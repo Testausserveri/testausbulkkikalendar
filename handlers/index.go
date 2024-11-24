c1d802e0a53384eac0f4462144dbd335cc5713c9
Adventune <adventunemusics@gmail.com>
2024-11-24 16:28:50 +0200

    WIP oauth example from google

--- handlers/index.go
+++ handlers/index.go
@@ -5,12 +5,14 @@ import (
 	"net/http"
 )
 
+// Also init templates for use in handlers package
 var templates *template.Template
 
 func Init(templateGlob string) {
 	templates = template.Must(template.ParseGlob(templateGlob))
 }
 
+// Index site handler
 func Index(w http.ResponseWriter, r *http.Request) {
 	data := struct {
 		Title   string
