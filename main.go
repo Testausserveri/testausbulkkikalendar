7c0943689fc3209f2732f738ac2550e34af7e528
Adventune <adventunemusics@gmail.com>
2024-11-24 17:12:25 +0200

    WIP primitive login

--- main.go
+++ main.go
@@ -3,19 +3,20 @@ package main
 import (
 	"log"
 	"net/http"
+	"testausserveri/testausbulkkikalendar/constants"
 	"testausserveri/testausbulkkikalendar/handlers"
+	"testausserveri/testausbulkkikalendar/oauth"
 )
 
-const PORT = "8080"
-
 func main() {
+	oauth.Init()
+
 	// Parse all templates in the templates directory
 	handlers.Init("templates/*.html")
 	// Define the handlers
-
 	http.HandleFunc("/", handlers.Index)
 
 	// Start the server
-	log.Println("Server listening on :" + PORT)
-	http.ListenAndServe(":"+PORT, nil)
+	log.Println("Server listening on :" + constants.PORT)
+	http.ListenAndServe(":"+constants.PORT, nil)
 }
